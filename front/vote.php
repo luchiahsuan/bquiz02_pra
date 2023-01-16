b71cef19af6a0af71a3c28c67455a19a3acdf2f8
chiahsuanlu <lu2010_81509@yahoo.com.tw>
2023-01-16 15:22:50 +0800

    完成投票api製作

--- front/vote.php
+++ front/vote.php
@@ -1,6 +1,6 @@
 <?php
 $subject - $Que->find($$_GET['id']);
-$options = $Que->all(['parent'->$_GET['id']]);
+$options = $Que->all(['parent'=>$_GET['id']]);
 
 
 ?>
@@ -8,13 +8,13 @@ $options = $Que->all(['parent'->$_GET['id']]);
 
 <fieldset>
     <legend>目前位置：首頁 > 問卷調查 > <?= $subject['text']; ?></legend>
-    <form action="./api/vote.php">
+    <form action="./api/vote.php" method="post">
         <h3><?= $subject['text']; ?></h3>
         <?php
 
         foreach ($options as $opt) {
             echo "<p>";
-            echo "<input type='radio' value='{$opt['id']}'>";
+            echo "<input type='radio' name='opt' value='{$opt['id']}'>";
             echo $opt['text'];
             echo "</p>";
         }
